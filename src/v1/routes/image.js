1770e04dea1b345076a7a4643d5094b3eb23d4e1
Zworld <92575518+zworldinc@users.noreply.github.com>
2023-09-20 03:45:30 +0530

    mm

--- src/v1/routes/image.js
+++ src/v1/routes/image.js
@@ -6,19 +6,19 @@ const router = express.Router()
 
 // Google Storage /////////
 ///////////////////////////
-import { Image } from '../../middlewares/multer.js'
-import { uploadImage } from '../../utils/extentions.js'
+// import { Image } from '../../middlewares/multer.js'
+// import { uploadImage } from '../../utils/extentions.js'
 
-router.post('/', Image.single('image'), async function (req, res) {
-    const myFile = req.file
-    const { endUrl, publicUrl } = await uploadImage(
-        myFile,
-        'folder2/' + myFile.originalname
-    )
-    res.status(200).json({
-        message: 'Upload was successful',
-        data: { endUrl, publicUrl },
-    })
-})
+// router.post('/', Image.single('image'), async function (req, res) {
+//     const myFile = req.file
+//     const { endUrl, publicUrl } = await uploadImage(
+//         myFile,
+//         'folder2/' + myFile.originalname
+//     )
+//     res.status(200).json({
+//         message: 'Upload was successful',
+//         data: { endUrl, publicUrl },
+//     })
+// })
 
 export default router
